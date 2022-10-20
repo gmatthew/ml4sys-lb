0159187da68f975bc159238095bb14d7d0287662
Gerard Matthew <gerard.matthew@gmail.com>
2022-10-19 21:33:20 -0500

    Add basic app for round robin

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import requests
+
 from flask import Flask
+from .round_robin import RoundRobinLB
 
 app = Flask(__name__)
 
+# These are just placeholders for now
+hosts = ["https://www.google.com", "https://www.cnn.com", "https://www.illinois.edu"]
+
+loadbalancer = RoundRobinLB(hosts)
+
 @app.route("/")
-def hello_world():
-    return "<p>Welcome to the Loadbalancer App</p>"
+def main():
+
+  host = loadbalancer.get_host()
+  request = requests.get(host)
+
+  return request.text
