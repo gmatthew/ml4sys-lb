cec63c8e1c7ea8ceb778f3ba4c0f04f1d244b478
Devi <dr15@illinois.edu>
2022-11-25 15:18:13 -0600

    Fixing errors with new code structure in fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -14,28 +14,26 @@ PERCENTAGE_KEY = 'percentage'
 
 DATA_RETRIEVAL_INTERVAL_SECONDS = 5
 
-def retrieve_node_stats():
-  node_stats = {}
+#retrieves data from the collectors
+def retrieve_stats():
+  collected_stats = {}
 
   for node_name in NODES:
     ip_address = NODES[node_name]
     r = requests.get('http://' + ip_address + ':5000')
     stats = json.loads(r.text)
 
-    node_stats[node_name] = stats
+    collected_stats[node_name] = stats
 
-  return node_stats
-
-'''
-returns the sort list of nodes to select from
-'''
+  return collected_stats
 
 
-def select_node(node_stats):
+#returns the list of nodes
+def select_node(collected_stats):
   node_data_list = []
 
-  for node_name in node_stats:
-    stats = node_stats[node_name]
+  for node_name in collected_stats:
+    stats = collected_stats[node_name]
     node = stats['node'][0]
 
     data = {}
@@ -48,22 +46,21 @@ def select_node(node_stats):
   return selection_algorithm(node_data_list)
 
 
+#returns the list of containers running in the first choice node 
 def select_container(stats):
-  # iterate over tats to create structure
-  # [ { name: 'foo', cpu: 10, memory: 20}]
   container_stats = []
 
   for i in range(0, len(stats)):
     data = {}
     data['name'] = stats[i]['container']
     data[CPU_KEY] = replace_percentage_sign(stats[i][CPU_KEY])
-    data[MEMORY_KEY] = replace_percentage_sign(stats[MEMORY_KEY]['percent'])
+    data[MEMORY_KEY] = replace_percentage_sign(stats[i][MEMORY_KEY]['percent'])
 
     container_stats.append(data)
 
   return selection_algorithm(container_stats)
 
-
+#returns the top choice of stats list (container or node)
 def selection_algorithm(stats):
   # stats argument structure => [ { name: 'foo', cpu: 10, memory: 20}]
   cpu_stats = []
@@ -86,14 +83,19 @@ def selection_algorithm(stats):
 
   dictionary_keys = list(diff.keys())
   sorted_diff_dict = {dictionary_keys[i]: sorted(diff.values())[i] for i in range(len(dictionary_keys))}
+  sorted_list = list(sorted_diff_dict.keys())
 
   ## @TODO - This should return a list instead of a sorted dictionary.
-  # return sorted list of name and weights
-  return sorted_diff_dict
+  # Returns the top choice or value at index 0
+  return sorted_list[0]
 
 
-def write_results(results):
-  print(results);
+#writes first choice node and container name to results file
+def write_results(node_name, container_name):
+  f = open("result.txt", "w")
+  f.write("node: " + node_name)
+  f.write("\ncontainer: " + container_name)
+  f.close()
 
 
 def replace_percentage_sign(value):
@@ -101,40 +103,11 @@ def replace_percentage_sign(value):
 
 
 def main():
-  node_stats = retrieve_node_stats();
-  selected_node_name = select_node(node_stats)[0]['name']
-  selected_container = select_container(node_stats[selected_node_name]['containers'])
+  collected_stats = retrieve_stats();
+  selected_node = select_node(collected_stats)
+  selected_container = select_container(collected_stats[selected_node]['containers'])
+  write_results(selected_node, selected_container)
 
-  write_results([])
-
-
-# -------------------------------------------------------------------------------
-#
-# node_containers_stats = {}
-# for node_name in nodes:
-#   node_containers_stats_list = []
-#   if (len(node_cont_stats[node_name]) != 0):
-#     for i in range(len(node_cont_stats[node_name])):
-#       l = {}
-#       l['name'] = node_cont_stats[node_name][i]['container']
-#       l[CPU_KEY] = node_cont_stats[node_name][i][CPU_KEY]
-#       l[MEMORY_KEY] = node_cont_stats[node_name][i][MEMORY_KEY]['percent']
-#       node_containers_stats_list.append(l)
-#
-#   # Dict with key =  node_name and value = list of containers with corresponding stats
-#   node_containers_stats[node_name] = node_containers_stats_list
-#
-# print(node_containers_stats)
-# sorted_node_list = selection_algorithm(node_stats)
-#
-# # Sorted node_list
-# print(sorted_node_list)
-# sorted_cont_list = {}
-# for node_name in sorted_node_list:
-#   sorted_cont_list[node_name] = selection_algorithm(node_containers_stats[node_name])
-
-# Sorted container list per node
-# print(sorted_cont_list)
 
 if __name__ == "__main__":
   try:
